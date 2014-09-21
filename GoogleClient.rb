b4b6e565d0c38d046ae92fee91a9a236763711c7
abeyuya <yuya.abe.0525@gmail.com>
2014-09-21 18:08:32 +0900

    とりあえず動くとこまで

---
+++ GoogleClient.rb
@@ -0,0 +1,43 @@
+# encoding: utf-8
+#!/usr/bin/ruby
+
+require 'google/api_client'
+require 'pp'
+
+class GoogleClient
+
+  def self.create_client
+
+    client = Google::APIClient.new(:application_name => 'youtube-survey')
+    key = Google::APIClient::PKCS12.load_key('config/youtube-survey-key.p12', 'notasecret')
+    client.authorization = Signet::OAuth2::Client.new(
+      :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+      :audience             => 'https://accounts.google.com/o/oauth2/token',
+      :scope                => 'https://www.googleapis.com/auth/youtube',
+      :issuer               => '984325115979-g9uug2fhjnp3oa68br34uegtoaqk40ii@developer.gserviceaccount.com',
+      :signing_key          => key
+    )
+    client.authorization.fetch_access_token!
+    youtube = client.discovered_api('youtube', 'v3')
+
+    return client, youtube
+  end
+
+  def search_video
+
+    google_client, youtube_client = GoogleClient.create_client
+
+    search_response_json = google_client.execute!(
+      :api_method => youtube_client.search.list,
+      :parameters => {
+        :part => 'snippet',
+        :q => 'google',
+        :maxResults => 25
+      }
+    )
+
+    search_result = JSON.parse(search_response_json.response.body)
+    pp search_result
+  end
+
+end
---
+++ main.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+#!/usr/bin/ruby
+
+
+load 'GoogleClient.rb'
+
+google_client = GoogleClient.new
+
+google_client.search_video
